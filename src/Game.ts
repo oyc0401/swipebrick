4cdebe3fa605c524190437d21b44f992c004830c
yuchan <oyc0401@gmail.com>
2025-09-25 14:32:20 +0900

    refactor: view 분리

---
+++ src/Game.ts
@@ -0,0 +1,37 @@
+import { GameState } from "./GameState";
+import { Ball } from "./entity/Ball";
+import { GameRenderer } from "./GameRenderer";
+import { PhysicsEngine } from "./PhysicsEngine";
+import { GameView } from "./view/GameView";
+
+export class Game {
+  private renderer: GameRenderer;
+  private physics: PhysicsEngine;
+  private gameState: GameState;
+  private ball: Ball;
+  private gameView: GameView;
+
+  constructor() {
+    this.renderer = new GameRenderer();
+    this.physics = new PhysicsEngine();
+    this.gameState = new GameState();
+    this.ball = new Ball(
+      this.gameState.ballStartPosition,
+      this.physics.getWorld()
+    );
+    this.gameView = new GameView(this.renderer, this.physics, this.ball);
+  }
+
+  public async init(): Promise<void> {
+    await this.renderer.init();
+    this.gameView.init();
+    this.startGameLoop();
+  }
+
+  private startGameLoop(): void {
+    this.renderer.getApp().ticker.add(() => {
+      this.physics.update(16);
+      this.ball.updateGraphics();
+    });
+  }
+}
---
+++ src/view/GameView.ts
@@ -0,0 +1,85 @@
+import * as PIXI from "pixi.js";
+import { GameRenderer } from "../GameRenderer";
+import { GameBoundary } from "../entity/GameBoundary";
+import { Ball } from "../entity/Ball";
+import { PhysicsEngine } from "../PhysicsEngine";
+
+const GAME_WIDTH = 360;
+const GAME_HEIGHT = 360;
+
+export class GameView {
+  private renderer: GameRenderer;
+  private physics: PhysicsEngine;
+  private ball: Ball;
+
+  private topBoundary!: GameBoundary;
+  private bottomBoundary!: GameBoundary;
+  private leftBoundary!: GameBoundary;
+  private rightBoundary!: GameBoundary;
+
+  constructor(renderer: GameRenderer, physics: PhysicsEngine, ball: Ball) {
+    this.renderer = renderer;
+    this.physics = physics;
+    this.ball = ball;
+  }
+
+  public init(): void {
+    this.addGameBoundaries();
+    this.addBallToScene();
+    this.addClickListener();
+    this.renderer.addDebugGuide();
+  }
+
+  private addGameBoundaries(): void {
+    const world = this.physics.getWorld();
+    const centerLayer = this.renderer.getCenterLayer();
+
+    this.topBoundary = new GameBoundary(0, -5, GAME_WIDTH, 5, 0x000000, world);
+    centerLayer.addChild(this.topBoundary.getGraphics());
+
+    this.bottomBoundary = new GameBoundary(
+      0,
+      GAME_HEIGHT,
+      GAME_WIDTH,
+      5,
+      0x000000,
+      world,
+      "bottom"
+    );
+    centerLayer.addChild(this.bottomBoundary.getGraphics());
+
+    this.leftBoundary = new GameBoundary(
+      -5,
+      0,
+      5,
+      GAME_HEIGHT,
+      0x000000,
+      world
+    );
+    centerLayer.addChild(this.leftBoundary.getGraphics());
+
+    this.rightBoundary = new GameBoundary(
+      GAME_WIDTH,
+      0,
+      5,
+      GAME_HEIGHT,
+      0x000000,
+      world
+    );
+    centerLayer.addChild(this.rightBoundary.getGraphics());
+  }
+
+  private addBallToScene(): void {
+    this.renderer.getGameViewport().addChild(this.ball.getGraphics());
+  }
+
+  private addClickListener(): void {
+    const gameViewport = this.renderer.getGameViewport();
+    gameViewport.eventMode = "static";
+    gameViewport.on("pointerdown", (event: PIXI.FederatedPointerEvent) => {
+      const localPosition = event.getLocalPosition(gameViewport);
+      console.log("Clicked at:", localPosition.x, localPosition.y);
+      this.ball.moveTowards(localPosition.x, localPosition.y);
+    });
+  }
+}
