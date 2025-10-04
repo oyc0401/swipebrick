745579569c4840c6b2c2f2019f3948baa185b441
yuchan <oyc0401@gmail.com>
2025-10-05 04:37:40 +0900

    build: 토스앱 라이브러리 더미 추가2

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
   resolve: {
     alias: {
       // 웹 빌드시 TDS 패키지를 더미로 대체
-      // "@toss/tds-mobile": "/src/utils/tds-dummy.tsx",
-      // "@toss/tds-mobile-ait": "/src/utils/tds-dummy.tsx",
-      // "@toss/tds-colors": "/src/utils/tds-dummy.tsx",
+      "@toss/tds-mobile": "/src/utils/tds-dummy.tsx",
+      "@toss/tds-mobile-ait": "/src/utils/tds-dummy.tsx",
+      "@toss/tds-colors": "/src/utils/tds-dummy.tsx",
     },
   },
 });
