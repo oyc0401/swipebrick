bd65876db37d355d8b746760267429addc16aa65
yuchan <oyc0401@gmail.com>
2025-10-08 05:30:56 +0900

    feat: 상점, 설정 버튼 삭제

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -49,26 +49,21 @@ export function Header() {
       <div css={headerWrapperStyle}>
         <div css={headerContentStyle}>
           <div css={buttonGroupStyle}>
-            <IconButton
-              iconSrc={rankIcon}
-              iconAlt="Ranking"
-              onClick={() => console.log("Ranking button clicked")}
-            />
+            {/* <IconButton
+              iconSrc={storeIcon}
+              iconAlt="Store"
+              onClick={() => console.log("Store button clicked")}
+            /> */}
           </div>
 
           <ScoreDisplay />
 
           <div css={rightButtonGroupStyle}>
             <IconButton
-              iconSrc={storeIcon}
-              iconAlt="Store"
-              onClick={() => console.log("Store button clicked")}
+              iconSrc={rankIcon}
+              iconAlt="Ranking"
+              onClick={() => console.log("Ranking button clicked")}
             />
-            {/* <IconButton
-              iconSrc={settingIcon}
-              iconAlt="Settings"
-              onClick={() => console.log("Settings button clicked")}
-            /> */}
           </div>
         </div>
       </div>
